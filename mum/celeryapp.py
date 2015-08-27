cb7630998506780b477fb80f7b3fba294bacf3bb
Alexey Strokach <alex.strokach@utoronto.ca>
2015-08-27 19:29:48 -0400

    Added a hack to circumvent a bug in celery: https://github.com/celery/celery/issues/1709
    Former-commit-id: 0e3611c4952623d5cb503919abe37b2d8df253f6

--- mum/celeryapp.py
+++ mum/celeryapp.py
@@ -3,6 +3,8 @@ from __future__ import absolute_import
 import os 
 
 from celery import Celery 
+from celery.signals import worker_process_init
+
 from django.conf import settings
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mum.settings')
@@ -10,6 +12,16 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mum.settings')
 app = Celery('mum') 
 app.config_from_object('django.conf:settings') 
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS) 
+
+
+@worker_process_init.connect
+def fix_multiprocessing(**kwargs):
+    from multiprocessing import current_process
+    try:
+        current_process()._config
+    except AttributeError:
+        current_process()._config = {'semprefix': '/mp'}
+
 
 @app.task(bind=True)
 def debug_task(self): 
