477a6f2efb2ea6b5be758004b5af449e7547aaa9
Alexey Strokach <alex.strokach@utoronto.ca>
2015-09-11 22:50:55 -0400

    Added all files required to build a vagrant devbox
    Former-commit-id: fdd2318230ff82a6ce222b24711fabfce72e9e46

---
+++ vagrant/install_python.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Make sure we know where python is installed
+if [[ -z "$CONDA_INSTALL_DIR" ]] ; then
+    echo '$CONDA_INSTALL_DIR must be set!'
+    exit 1
+fi
+if [[ ! "$PATH" =~ "$CONDA_INSTALL_DIR" ]] ; then
+    export PATH=$CONDA_INSTALL_DIR/bin:$PATH
+fi
+
+# Install miniconda
+if [[ ! -e "$CONDA_INSTALL_DIR/bin/python" ]] ; then
+    echo "Installing Miniconda..."
+    rm -rf "$CONDA_INSTALL_DIR"
+    mkdir "$CONDA_INSTALL_DIR"
+    wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh --quiet
+    bash Miniconda3-latest-Linux-x86_64.sh -b -f -p "$CONDA_INSTALL_DIR"
+    rm -f Miniconda3-latest-Linux-x86_64.sh
+fi
+
+# Add custom channels to ~/.condarc
+cat <<EOF > $HOME/.condarc
+channels:
+  - ostrokach
+  - defaults
+EOF
+
+# Install useful conda packages
+conda update -y -q conda
+conda install -y -q anaconda-client
+conda install -y -q conda-build jinja2 pip
+pip install mod_wsgi
+
