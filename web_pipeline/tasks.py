5286df900d9c2525bb6c8a4006e1ce9541e7789c
Alexey Strokach <alex.strokach@utoronto.ca>
2015-12-01 23:12:06 -0500

    Use REST API instead of socketserver and convert from DOS(?) to Unix formatting (to be consistent with everything else
    Former-commit-id: cb2d90e1e886906da025fce5d331717f75e91b17

--- web_pipeline/tasks.py
+++ web_pipeline/tasks.py
@@ -1,4 +1,4 @@
-from __future__ import absolute_import 
+from __future__ import absolute_import
 
 
 import shutil
@@ -29,32 +29,32 @@ from web_pipeline.elaspic_socket_client import JobSubmitter
 
 @app.task
 def cleanupServer():
-    
+
     c = CleanupManager()
-    
+
     # Remove jobs last visited too long ago.
     c.removeOldJobs()
-    
+
 #    c.checkForStalledMuts()
-    
+
     # Restart stalled jobs, and delete orphan mutations still running/queued.
 #    m_runAgain = c.checkForStalledJobs()
 #    for m in m_runAgain:
 #        runPipelineWrapper.delay(m[0], m[1])
-    
+
     # Send crash logs to admins.
     c.sendCrashLogs()
-    
+
 #    return len(m_runAgain)
 
 @app.task
 def sleepabit(a=5, b=10):
-    
+
     sleepFor = randint(a, b)
     print("Will now sleep for %d seconds." % sleepFor)
     sleep(sleepFor)
     print("Done sleeping!")
-    
+
     return 1
 
 
@@ -64,7 +64,7 @@ def _run_pipeline_locally(mutation, logger):
     pipeline(mutation.protein, mutation.mut, run_type=5)
 
 
-def _run_pipeline_remotely(mutation, logger):
+def _run_pipeline_remotely_old(mutation, logger):
     js = JobSubmitter(logger=logger)
 
     logger.debug("uniprot_id: {}, mutations: {}".format(mutation.protein, mutation.mut))
@@ -79,24 +79,42 @@ def _run_pipeline_remotely(mutation, logger):
     #TODO: There are better ways to handle job_ids (read files in finished/ folder),
     #but we don't need them for now...
     return output_dict
-    
+
+
+def _run_pipeline_remotely(mutation, logger):
+    import requests
+    url = 'http://192.168.6.201:8890/elaspic/api/v1.0/submitjob'
+
+    logger.debug("uniprot_id: {}, mutations: {}".format(mutation.protein, mutation.mut))
+    r = requests.post(url, {'uniprot_id': mutation.protein, 'mutations': mutation.mut})
+    logger.debug('post response: {}'.format(r.json()))
+
+    time.sleep(5)
+    r = requests.get(url, {'uniprot_id': mutation.protein, 'mutations': mutation.mut})
+    logger.debug('output_dict: {}'.format(r.json()))
+    while 'result' not in r.json()[mutation.mut] or not r.json()[mutation.mut]['result']:
+        time.sleep(60)
+        r = requests.get(url, {'uniprot_id': mutation.protein, 'mutations': mutation.mut})
+        logger.debug('output_dict: {}'.format(r.json()))
+    return r.json()[mutation.mut]
+
 
 @app.task(rate_limit='10/m', max_retries=2)
 def runPipelineWrapper(mutation, jid):
-    
+
     # Set mutation status as running.
     if not mutation.rerun:
         mutation.status = 'running'
     else:
         mutation.rerun = 2
     mutation.save()
-        
-    
+
+
     # Change current folder to pipeline code.
     #~ tempDir = "elaspic_%s_%s_%s/" % (jid, mutation.protein, mutation.mut)
     #~ pipelineDir = os.path.join(settings.PROJECT_ROOT, 'pipeline/code/')
     #~ os.chdir(pipelineDir)
-    #~ 
+    #~
     # Create temp config file.
     #~ t = get_template('configTemp.ini')
     #~ c = Context({'tempDir': tempDir,
@@ -133,7 +151,7 @@ def runPipelineWrapper(mutation, jid):
     logger = logging.getLogger(logName)
     hdlr = logging.FileHandler(logFile.name)
     hdlr.setFormatter(logging.Formatter('%(asctime)s [%(levelname)s]: %(message)s'))
-    logger.addHandler(hdlr) 
+    logger.addHandler(hdlr)
     logger.setLevel(logging.DEBUG)
     logger.propagate = False
 
@@ -142,23 +160,23 @@ def runPipelineWrapper(mutation, jid):
 
     try:
         logger.info("mutation: {}".format(mutation))
-        
+
         ### Run pipeline ###
         #_run_pipeline_locally(mutation, logger)
         output_dict = _run_pipeline_remotely(mutation, logger)
-        
+
         logger.debug("Saving job ids to the database...")
-        mutation.provean_job_id = output_dict.get('provean_job_id')
-        mutation.model_job_id = output_dict.get('model_job_id')
-        mutation.mutation_job_id = output_dict.get('mutation_job_id')
-        
+        mutation.provean_job_id = output_dict['sequence'].get('job_id')
+        mutation.model_job_id = output_dict['model'].get('job_id')
+        mutation.mutation_job_id = output_dict[mutation.mut].get('job_id')
+
     except SoftTimeLimitExceeded:
         # Out of time. Cleanup!
         mutation.status = 'error'
         mutation.affectedType = ''
         mutation.error = '3: OUTATIME'
         exitCode = 3
-        
+
     except Exception as e:
         # Other unknown exception.
         mutation.status = 'error'
@@ -170,14 +188,14 @@ def runPipelineWrapper(mutation, jid):
         logger.error(trace)
         logFile.flush()
         shutil.copy(logFile.name, os.path.join(settings.CRASH_LOG_PATH, logName + '.log'))
-        
+
         exitCode = 2
-    
+
     else:
         # Fetch completed mutations.
         mut = list(Mutation.objects.using('data').filter(protein_id=mutation.protein, mut=mutation.mut))
         imut = list(Imutation.objects.using('data').filter(protein_id=mutation.protein, mut=mutation.mut))
-        
+
         # Check if an error occoured.
         coreOrInt, muts = ('IN', imut) if imut else ('CO', mut)
         if mut + imut:
@@ -214,16 +232,16 @@ def runPipelineWrapper(mutation, jid):
             mutation.status = 'done'
             mutation.error = None
             mutation.affectedType = 'NO'
-    
-    
+
+
     finally:
-        
+
         # Fail-safe in case of sys.exit() or some other dangerous stuff is
         # called within the pipeline.
         if exitCode == -1:
             mutation.status = 'error'
             mutation.error = '2: PIPELINECRASH: EXIT STATUS 4'
-        
+
         # (Save and) Remove logger
         if settings.DEBUG:
             logFile.flush()
@@ -234,7 +252,7 @@ def runPipelineWrapper(mutation, jid):
         # Set current working directory and environment back to default.
         os.chdir(os.path.join(settings.PROJECT_ROOT, '..'))
         #~ os.environ = old_env
-        
+
         # Save mutation.
         mutation.dateFinished = now()
         mutation.rerun = False
@@ -254,7 +272,7 @@ def runPipelineWrapper(mutation, jid):
 #            shutil.rmtree('/tmp/' + tempDir)
 #        except Exception:
 #            pass
-        
+
         # Return.
         # 0: Complete without errors.
         # 1: Complete with errors.
@@ -264,8 +282,8 @@ def runPipelineWrapper(mutation, jid):
         # 5: Blacklisted protein.
         return exitCode if exitCode == -1 else 4
 
-    
-        
+
+
 
 @app.task(rate_limit='10/m', max_retries=2)
 def runPipelineWrapperAll(mutations, jid):
@@ -274,14 +292,14 @@ def runPipelineWrapperAll(mutations, jid):
     """
     if not isinstance(mutations, list):
         mutations = [mutations]
-        
+
     # Group mutations
     mutation_dict = dict()
     for mutation in mutations:
         mutation_dict.setdefault(mutation.protein, set()).add(mutation.mut)
     for key in mutation_dict:
         mutation_dict[key] = ','.join(mutation_dict[key])
-        
+
     # Set mutation status as running.
     for mutation in mutations:
         if not mutation.rerun:
@@ -289,15 +307,15 @@ def runPipelineWrapperAll(mutations, jid):
         else:
             mutation.rerun = 2
         mutation.save()
-    
-    
+
+
     # Create logger to redirect output.
     logName = "%s" % localtime(now()).strftime('%Y-%m-%d_%H.%M.%S')
     logFile = NamedTemporaryFile()
     logger = logging.getLogger(logName)
     hdlr = logging.FileHandler(logFile.name)
     hdlr.setFormatter(logging.Formatter('%(asctime)s [%(levelname)s]: %(message)s'))
-    logger.addHandler(hdlr) 
+    logger.addHandler(hdlr)
     logger.setLevel(logging.DEBUG)
     logger.propagate = False
 
@@ -310,7 +328,7 @@ def runPipelineWrapperAll(mutations, jid):
 
 
     try:
-        
+
         ### Run pipeline ###
         #_run_pipeline_locally(mutation, logger)
         mutation_dict_output = _run_pipeline_remotely(mutation_dict, logger)
@@ -320,7 +338,7 @@ def runPipelineWrapperAll(mutations, jid):
 #            mutation.model_job_id = mutation_dict_output[mutation.protein].get('model_job_id')
 #            mutation.mutation_job_id = mutation_dict_output[mutation.protein].get('mutation_job_id')
 
-        # Save logs if we're debugging        
+        # Save logs if we're debugging
         if settings.DEBUG:
             logFile.flush()
             shutil.copy(logFile.name, os.path.join(settings.ELASPIC_LOG_PATH, logName + '.log'))
@@ -332,7 +350,7 @@ def runPipelineWrapperAll(mutations, jid):
             mutation.affectedType = ''
             mutation.error = '3: OUTATIME'
             exitCode = 3
-        
+
     except Exception as e:
         # Other unknown exception.
         for mutation in mutations:
@@ -345,16 +363,16 @@ def runPipelineWrapperAll(mutations, jid):
         logger.error(trace)
         logFile.flush()
         shutil.copy(logFile.name, os.path.join(settings.CRASH_LOG_PATH, logName + '.log'))
-        
+
         exitCode = 2
-    
+
     else:
         # Fetch completed mutations.
         logger.debug("Going over every mutation to check if it's been calculcated correctly...")
         for mutation in mutations:
             mut = list(Mutation.objects.using('data').filter(protein_id=mutation.protein, mut=mutation.mut))
             imut = list(Imutation.objects.using('data').filter(protein_id=mutation.protein, mut=mutation.mut))
-            
+
             # Check if an error occoured.
             coreOrInt, muts = ('IN', imut) if imut else ('CO', mut)
             if mut + imut:
@@ -375,9 +393,9 @@ def runPipelineWrapperAll(mutations, jid):
                 mutation.status = 'done'
                 mutation.error = None
                 mutation.affectedType = 'NO'
-                
+
         logger.debug('mutations: {}'.format(mutations))
-    
+
     finally:
         logger.debug("Check that everything is complete and send out emails...")
         for mutation in mutations:
@@ -388,21 +406,21 @@ def runPipelineWrapperAll(mutations, jid):
                 mutation.status = 'error'
                 mutation.error = '2: PIPELINECRASH: EXIT STATUS 4'
             logger.debug("Exit code: {}".format(exitCode))
-            
+
             # Save mutation.
             mutation.dateFinished = now()
             mutation.rerun = False
             logger.debug("Saving mutation stats...")
             mutation.save()
             logger.debug("Done saving mutation stats...")
-        
+
             # If all mutations in job are done, then set job as done.
             logger.debug("checkForCompletion")
             logger.debug("mutation jobs: {}".format(mutation.jobs.all()))
-            
+
             logFile.flush()
             shutil.copy(logFile.name, os.path.join(settings.ELASPIC_LOG_PATH, logName + '.log'))
-            
+
             checkForCompletion(mutation.jobs.all())
 
         # Remove logger.
@@ -420,7 +438,7 @@ def runPipelineWrapperAll(mutations, jid):
             os.environ['MPLCONFIGDIR'] = ''
         except Exception:
             pass
-        
+
         # Return.
         # 0: Complete without errors.
         # 1: Complete with errors.
