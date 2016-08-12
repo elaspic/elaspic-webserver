0db147c5a61d8fee1f9d4bf8347faef344af0a47
Alexey Strokach <alex.strokach@utoronto.ca>
2016-08-11 20:42:57 -0400

    Updated 'create_conda_env.sh'.

--- create_conda_env.sh
+++ create_conda_env.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+set -ex
+
+sudo apt install apache2-dev
+
 conda create -n mum 'python=3.5' elaspic django
-source activate -n mum
+source activate mum
 pip install mod_wsgi aiohttp aiomysql
 
