727bebe2c5c1c02359c59a62b6ef7318011b3e3d
Alexey Strokach <alex.strokach@utoronto.ca>
2016-08-11 20:48:35 -0400

    Display both core and interface mutations. Fix filemanager code. Fix bug where mutation was not shown while it was being calculated.

--- mum/processors.py
+++ mum/processors.py
@@ -1,7 +1,8 @@
 from django.conf import settings
 
+
 def sitevars(HttpRequest):
-    return {'SITE_NAME': settings.SITE_NAME, 
+    return {'SITE_NAME': settings.SITE_NAME,
             'STATIC_FOLDER': settings.STATIC_URL,
             'JOB_EXPIRY_DAY': settings.JOB_EXPIRY_DAY,
-            'TIME_LIMIT': settings.CELERYD_TASK_SOFT_TIME_LIMIT/3600}
+            'TIME_LIMIT': settings.TASK_SOFT_TIME_LIMIT / 3600}
