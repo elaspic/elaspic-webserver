1e5cc1587541938d834f66dac3e5b30643afd266
Alexey Strokach <alex.strokach@utoronto.ca>
2016-09-05 18:48:40 -0400

    Install mysqlclient through conda.

--- deploy/create_conda_env.sh
+++ deploy/create_conda_env.sh
@@ -4,6 +4,7 @@ set -ex
 
 sudo apt install apache2-dev postfix  # to send mail
 
-conda create -n elaspic_webserver 'python=3.5' elaspic django
+conda create -n elaspic_webserver 'python=3.5' elaspic django mysqlclient
 source activate elaspic_webserver
 pip install mod_wsgi aiohttp aiomysql pytest pytest-asyncio
+
