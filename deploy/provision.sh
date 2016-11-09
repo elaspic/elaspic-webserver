91ce6d1e0d768dd5cd1bdfe6834cfa5467e8e5f9
Alexey Strokach <alex.strokach@utoronto.ca>
2016-11-09 23:15:47 +0000

    Changes required to get MUM to work on the deployment VM.

--- deploy/provision.sh
+++ deploy/provision.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
-rsync -av strokach@192.168.6.201:/home/kimlab1/strokach/websites/mum/ ./mum \
-    --exclude /static --exclude /log --exclude __pycache__
+set -ev
 
-python ./mum/manage.py collectstatic --noinput -i download -i jobs
-sudo ln -sf \
+# Jobsubmitter
+rsync -rv --chown=9284:7300 ~/mum/jobsubmitter/scripts/ /home/kimlab1/database_data/elaspic_v2/scripts/
+
+sudo supervisorctl restart all
+
+
+# ELASPIC
+python ~/mum/manage.py collectstatic --noinput -i download -i jobs
+
+sudo ln -snf \
     /home/kimlab1/database_data/elaspic_v2/webserver/jobs \
     /var/www/elaspic.kimlab.org/static/jobs
-sudo ln -sf \
+
+sudo ln -snf \
     /home/kimlab1/database_data/elaspic.kimlab.org \
     /var/www/elaspic.kimlab.org/static/download
 
-# sudo supervisorctl restart all
 sudo service apache2 restart
+
