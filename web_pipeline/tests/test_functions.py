34d918dcc1718b69b4bd21c895c2724ce7a12b76
Alexey Strokach <alex.strokach@utoronto.ca>
2016-12-02 21:23:32 -0500

    Add more web_pipeline tests.

---
+++ web_pipeline/tests/test_functions.py
@@ -0,0 +1,15 @@
+import io
+import logging
+import os.path as op
+import tempfile
+
+import pandas as pd
+import pytest
+# from hypothesis import given
+# from hypothesis.strategies import text
+from faker import Faker
+
+import functions
+
+logger = logging.getLogger()
+fake = Faker()
