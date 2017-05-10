79b236b1d2b4d5b67b0f80415ddbcd3ac2a1c49f
Alexey Strokach <alex.strokach@utoronto.ca>
2017-05-10 08:49:23 -0400

    Create gitlab-ci folder

--- devtools/gitlab-ci/sentry_release.sh
+++ devtools/gitlab-ci/sentry_release.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-set -e
+set -ex
 
 SENTRY_URL="https://sentry.io/api/hooks/release/builtin/129313/3284ec114189c6eac1528760d12a29ceda54ecf39ca9402ff0e2dcf831a5be95/"
 
 curl "${SENTRY_URL}" \
   -X POST \
   -H 'Content-Type: application/json' \
-  -d "{\"version\": $(git log -n 1 --pretty=format:%H)}"
+  -d "{\"version\": \"$(git log -n 1 --pretty=format:%H)\"}"
