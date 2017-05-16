1ec49954e8349c140d84d882967caf859fc9e2c2
Alexey Strokach <alex.strokach@utoronto.ca>
2017-05-16 15:18:17 -0400

    Configure raven in django project only
    Otherwise get duplicate messages

--- devtools/gitlab-ci/sentry_release.sh
+++ devtools/gitlab-ci/sentry_release.sh
@@ -2,9 +2,9 @@
 
 set -ex
 
-SENTRY_URL="https://sentry.io/api/hooks/release/builtin/129313/3284ec114189c6eac1528760d12a29ceda54ecf39ca9402ff0e2dcf831a5be95/"
+ELASPIC_WEBSERVER_SENTRY_URL="https://sentry.io/api/hooks/release/builtin/129313/3284ec114189c6eac1528760d12a29ceda54ecf39ca9402ff0e2dcf831a5be95/"
 
-curl "${SENTRY_URL}" \
+curl "${ELASPIC_WEBSERVER_SENTRY_URL}" \
   -X POST \
   -H 'Content-Type: application/json' \
   -d "{\"version\": \"$(git log -n 1 --pretty=format:%H)\"}"
