30944f48dfd0faeb91ba2301f698fb9e0bf72747
Alexey Strokach <alex.strokach@utoronto.ca>
2017-05-16 18:59:21 -0400

    Allways set database name to 'pytest...' when testing
    Previously, this was only the case when DJANGO_SETTINGS_MODULE was not set.

--- conftest.py
+++ conftest.py
@@ -1,35 +1,8 @@
 import os
-import os.path as op
-import sys
 
-
-BASE_DIR = op.dirname(op.abspath(__file__))
-print(BASE_DIR)
-
-if BASE_DIR not in sys.path:
-    sys.path.insert(0, BASE_DIR)
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mum.settings.pytest')
+os.environ['DJANGO_SETTINGS_MODULE'] = 'mum.settings.pytest'
 
 import django
 django.setup()
 
-
-# import pytest
-#
-# @pytest.fixture(scope='session')
-# def django_db_setup():
-#     """Avoid creating/setting up the test database.
-#
-#     Source: http://pytest-django.readthedocs.io/en/latest/database.html#use-a-read-only-database
-#     """
-#     pass
-#
-#
-# @pytest.fixture
-# def db_access_without_rollback_and_truncate(request, django_db_setup, django_db_blocker):
-#     """
-#     Source: http://pytest-django.readthedocs.io/en/latest/database.html#use-a-read-only-database
-#     """
-#     django_db_blocker.unblock()
-#     request.addfinalizer(django_db_blocker.restore)
+assert django.conf.settings.DATABASES['default']['NAME'].startswith('pytest')
