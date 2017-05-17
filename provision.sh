210d5643b0f056f0d9b592fd8b589c6e2bdc1228
Alexey Strokach <alex.strokach@utoronto.ca>
2017-05-17 23:58:37 +0000

    Changes required to work on pruduction

--- provision.sh
+++ provision.sh
@@ -1,19 +1,13 @@
 #!/bin/bash
 
-read -p "This will replace your configuration files! Are you sure? " -n 1 -r
-echo    # (optional) move to a new line
-if [[ ! $REPLY =~ ^[Yy]$ ]]
-then
-    # handle exits from shell or function but don't exit interactive shell:
-    [[ "$0" == "$BASH_SOURCE" ]] && exit 1 || return 1
+set -ex
+
+if [ "$(id -u)" == "0" ]; then
+    echo "This script must not be run as root!"
+    exit 1
 fi
 
-set -ev
-
-
 # Jobsubmitter
-
-rsync -rv --chown=9284:7300 ~/mum/jobsubmitter/scripts/ /home/kimlab1/database_data/elaspic/scripts/
 
 sudo rsync -av ./conf/jobsubmitter.conf /etc/supervisor/conf.d/jobsubmitter.conf
 
@@ -21,10 +15,9 @@ sudo supervisorctl reload
 sudo supervisorctl reread
 sudo supervisorctl restart jobsubmitter
 
-
 # ELASPIC
 
-python ~/mum/manage.py collectstatic --noinput -i download -i jobs
+DJANGO_SETTINGS_MODULE='mum.settings.production' /home/kimadmin/miniconda/envs/elaspic-webserver/bin/python manage.py collectstatic --noinput -i download -i jobs
 
 sudo ln -snf \
     /home/kimlab1/database_data/elaspic/webserver/jobs \
@@ -34,10 +27,13 @@ sudo ln -snf \
     /home/kimlab1/database_data/elaspic.kimlab.org \
     /var/www/elaspic.kimlab.org/static/download
 
-
 # Apache
 
 sudo rsync -av ./conf/elaspic_webserver.conf /etc/apache2/sites-available/elaspic_webserver.conf
-sudo ln -sf '../sites-available/elaspic_webserver.conf' '/etc/apache2/sites-enabled/elaspic_webserver.conf'
+
+pushd /etc/apache2/sites-enabled/
+  sudo ln -sf ../sites-available/elaspic_webserver.conf
+popd
 
 sudo service apache2 restart
+
