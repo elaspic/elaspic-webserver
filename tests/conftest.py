4ee4729bb89825c997a27cf9c38d933ddfbd28ba
ostrokach <ostrokach@gmail.com>
2020-12-22 00:03:39 -0500

    Move conftest.py to tests folder and patch DATA_DIR

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,16 +4,22 @@ from dotenv import load_dotenv
 
 load_dotenv(dotenv_path=os.getenv("ENV_FILE", ".env.test"), override=True)
 
+from pathlib import Path
+
 import django
 import pytest
 
 django.setup()
 
+# Database settings
 if not django.conf.settings.DATABASES["default"]["USER"].endswith("-readonly"):
     django.conf.settings.DATABASES["default"]["USER"] += "-readonly"
     django.conf.settings.DATABASES["default"]["PASSWORD"] += "-readonly"
 assert django.conf.settings.DATABASES["default"]["USER"].endswith("-readonly")
 assert django.conf.settings.DATABASES["default"]["PASSWORD"].endswith("-readonly")
+
+# Data dir
+django.conf.settings.DATA_DIR = Path(__file__).resolve(strict=True).parent.as_posix()
 
 
 @pytest.fixture(scope="session")
