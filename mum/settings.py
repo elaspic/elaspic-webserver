630284d463b9b4f315085ff6cdd7fa150ec8e330
Alexey Strokach <alex.strokach@utoronto.ca>
2021-03-31 21:01:22 +0000

    Set sentry release

--- mum/settings.py
+++ mum/settings.py
@@ -53,6 +53,7 @@ if SENTRY_DSN is not None:
         # If you wish to associate users to errors (assuming you are using
         # django.contrib.auth) you may enable sending PII data.
         send_default_pii=True,
+        release="elaspic-webserver@0.2.8",
     )
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
